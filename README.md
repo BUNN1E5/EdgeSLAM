0a4ae18428d76604c799068ba6f1f2cf3bf78063
Jacob <jacob@goldenneedham.com>
2020-07-17 03:07:44 -0700

    Added Comments, Rewrote README

--- README.md
+++ README.md
@@ -1,5 +1,26 @@
-# EdgeSLAM
+# SLAM Implementation
 
-This is an attemot at an implementation of a SLAM
-algorithm. I will be writing it in python, then rewriting it
-in C to allow for the use in openHMD
+In an effort to learn more about OpenCV I am making an attemped at writing a custom implementation of the SLAM algorithm
+
+## TODO 
+
+Basically Everything 0.o
+
+## About this implementation
+
+This is will be a single camera implementation of SLAM as I only have a single PS3 Eye camera right now. 
+
+Here are some ideas about the high level on how this algorithm is going to determine position
+
+Using ORB Feature matching we can actually compare between 2 different images, the current frame and the previous
+this should give us a basic idea about the opitcal flow of our camera by finding the average distance moved between frames.
+
+This isn't perfect however, becasue there is the possibility of moving objects within the frame. So we also have to remove any outlier
+or weigh the different points by some measure.
+
+
+Now there is also the problem that this method does not find any rotational movement, which is unfortunate, but as of right now I want to get this basic motion detection working.
+
+### Potential Ideas
+
+Can we use an edge detection algorithm to make it easier to find keypoints as well as remove a lot of noise?
--- SLAM_3AXIS/orbTest.py
+++ SLAM_3AXIS/orbTest.py
@@ -1,6 +1,9 @@
 import cv2
 import numpy as np
 
+#Right now I'm just testing out the ORB detection algorithm because I head that it is marginally faster than SIFT or SURF
+
+
 cap = cv2.VideoCapture(0)
 ret, frame = cap.read()
 ret, last = cap.read()
@@ -21,15 +24,26 @@ while(True):
     frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) 
     #Do the work here!
     
+    #Lets see how long the orb algorithm actually takes to calculate
+    #as long as we store the previous keypoints found we actually only ever need to run the detection once a frame
+    
     kp, des = orb.detectAndCompute(frame, None)
     img2 = cv2.drawKeypoints(frame,kp, outImage = None, color=(0,255,0), flags=0)
+    
+    #Another importatant thing to check is how long the matching takes
+    
     matches = bf.match(des, des_last)
-
     matches = sorted(matches, key = lambda x:x.distance)
     matches.reverse()
+    
+    #last we need to get the matched keypoints and find the average distance moved on them, however we do not know the distance of each point from the camera.
+    #solvePNP can actually be a bit slow so lets try and see if we can take advantage of knowing the focal length
+    
+    #If we know the focal length we could potentially use the difference in lateral movement of the keypoints to estimate distance from the camera with a sufficient amount of points
 
     img3 = cv2.drawMatches(frame,kp, last,kp_last, matches[:10], None,flags=2)
     # Display the resulting frame
+    
     cv2.imshow('frame', frame)
     cv2.imshow('kp',img2)
 
